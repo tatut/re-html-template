b4970c985508c796f69280663204bb3f14ee5df3
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-04-28 16:56:53 +0300

    Add clear completed and remove events

--- todomvc/src/cljs/todomvc/events.cljs
+++ todomvc/src/cljs/todomvc/events.cljs
@@ -5,7 +5,10 @@
 (defrecord AddTodo [])
 
 (defrecord ToggleCompleted [idx])
+(defrecord Remove [idx])
 (defrecord SetFilter [filter])
+
+(defrecord ClearCompleted [])
 
 (extend-protocol t/Event
   UpdateNewTodo
@@ -22,6 +25,18 @@
   (process-event [{idx :idx} app]
     (update-in app [:todos idx :completed?] not))
 
+  Remove
+  (process-event [{idx :idx} {todos :todos :as app}]
+    (assoc app :todos
+           (if (zero? idx)
+             (subvec todos 1)
+             (into (subvec todos 0 idx)
+                   (subvec todos (inc idx))))))
+
   SetFilter
   (process-event [{filter :filter} app]
-    (assoc app :filter filter)))
+    (assoc app :filter filter))
+
+  ClearCompleted
+  (process-event [_ app]
+    (update app :todos #(filterv (complement :completed?) %))))
